1c920bdd18e37d0355fc0f26094f218a445ebddb
kulorido <yangyanglitfriend@163.com>
2023-06-11 00:05:51 +0800

    线程数调整

---
+++ data_cs_service/src/main/java/org/kulorido/exception/DataSynchronizationBaseException.java
@@ -0,0 +1,20 @@
+package org.kulorido.exception;
+
+public class DataSynchronizationCoreException extends DataSynchronizationException{
+
+    public DataSynchronizationCoreException(String message) {
+        super(message);
+    }
+
+    public DataSynchronizationCoreException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+    public DataSynchronizationCoreException(String format, Object... args) {
+        super(format, args);
+    }
+    
+    public static void DataSynchronizationCoreException extends DataSynchronizationException{
+//        super;
+    }
+}
---
+++ data_cs_test/src/main/java/org/kulorido/any/Test2.java
@@ -0,0 +1,2 @@
+package org.kulorido.any;public class Test2 {
+}
